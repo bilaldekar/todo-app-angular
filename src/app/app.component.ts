2534d273cb6d20d7e1f0f8cc463bb2121090565e
bilal.dekar <bilal.dekar@cevital.com>
2021-01-28 15:37:13 +0100

    full screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,14 @@ export class AppComponent implements OnInit {
 
   elem;
   navOpened: any = true;
+  isFullScreen: any = false;
 
   constructor(private taskService: TaskService, private confirmationService: ConfirmationService, @Inject(DOCUMENT) private document: any) {
     this.taskService.number.subscribe((res: number) => this.number = res);
   }
 
   ngOnInit(): void {
+    this.elem = document.documentElement;
 
     if (!localStorage.getItem('tasks')) {
       localStorage.setItem('tasks', '{"tasks":[]}');
@@ -81,5 +83,53 @@ export class AppComponent implements OnInit {
     });
   }
 
+
+  openNav() {
+    this.document.getElementById("sidebar").style.width = "197px";
+    this.document.getElementById("main").style.marginLeft = "197px";
+    this.document.getElementById("header").style.marginLeft = "183px";
+    this.document.getElementById("header").style.width = "85%";
+    this.navOpened = true;
+  }
+
+  closeNav() {
+    this.document.getElementById("sidebar").style.width = "0";
+    this.document.getElementById("main").style.marginLeft = "0";
+    this.document.getElementById("header").style.marginLeft = "0";
+    this.document.getElementById("header").style.width = "100%";
+    this.navOpened = false;
+  }
+
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
+    this.isFullScreen = true;
+  }
+
+  closeFullscreen() {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
+    this.isFullScreen = false;
+  }
 }
 
